c741c868057ed5b7dfbf3b61560725ea76c83bde
M. Ramón López Torres <rlopez@kolokium.com>
2022-04-11 17:56:09 +0200

    add cryptopp module

--- cmake/abieos.cmake
+++ cmake/abieos.cmake
@@ -11,9 +11,11 @@ set(BUILD_TESTING 0)
 
 FetchContent_Declare(abieos
 	GIT_REPOSITORY https://github.com/EOSIO/abieos.git
-	GIT_TAG master)
+	GIT_TAG master
+)
 
 FetchContent_GetProperties(abieos)
+
 if(NOT abieos_POPULATED)
 	FetchContent_Populate(abieos)
 	add_subdirectory(${abieos_SOURCE_DIR} ${abieos_BINARY_DIR} EXCLUDE_FROM_ALL)
--- cmake/cryptopp.cmake
+++ cmake/cryptopp.cmake
@@ -5,23 +5,33 @@ if (NOT Git_FOUND)
 	message(FATAL_ERROR "Git not found!")
 endif ()
 
-include(ExternalProject)
-find_program(MAKE_EXE NAMES gmake nmake make)
+include(FetchContent)
 
-set (CRYPTOPP "cryptopp")
-ExternalProject_Add(
-	${CRYPTOPP}
-	PREFIX            ${CRYPTOPP}
+FetchContent_Declare(cryptopp
 	GIT_REPOSITORY https://github.com/weidai11/cryptopp.git
-
-	CONFIGURE_COMMAND ""
-	UPDATE_COMMAND    ""
-	INSTALL_COMMAND   ""
-
-	BUILD_IN_SOURCE   true
-	BUILD_ALWAYS      OFF
-	INSTALL_DIR       ${CMAKE_CURRENT_BINARY_DIR}/ext/${CRYPTOPP}
-
-	BUILD_COMMAND     sh -c "source TestScripts/setenv-ios.sh IOS_SDK=iPhone IOS_CPU=arm64 && ${MAKE_EXE}"
-	INSTALL_COMMAND   ${MAKE_EXE} DESTDIR=${CMAKE_CURRENT_BINARY_DIR}/ext/${CRYPTOPP} PREFIX=/ install
+	GIT_TAG master
 )
+
+FetchContent_GetProperties(cryptopp)
+
+if(NOT cryptopp_POPULATED)
+	FetchContent_Populate(cryptopp)
+
+	# Copy an additional/replacement file into the populated source
+	file(REMOVE_RECURSE ${cryptopp_SOURCE_DIR}/include)
+
+	file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/cmake/cryptopp/CMakeLists.txt DESTINATION ${cryptopp_SOURCE_DIR})
+
+	file(
+		COPY ${cryptopp_SOURCE_DIR}
+		DESTINATION ${cryptopp_SOURCE_DIR}/include
+		FILES_MATCHING 
+			PATTERN "*.h" 
+			PATTERN ".git*" EXCLUDE 
+			PATTERN "include" EXCLUDE 		
+	)
+
+	file(RENAME ${cryptopp_SOURCE_DIR}/include/cryptopp-src ${cryptopp_SOURCE_DIR}/include/cryptopp)
+	
+	add_subdirectory(${cryptopp_SOURCE_DIR} ${cryptopp_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
