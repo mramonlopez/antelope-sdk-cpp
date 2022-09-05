f9eadd48072cbd1d54a4e53a623c91b5f6369640
M. Ramón López Torres <rlopez@kolokium.com>
2022-09-05 13:39:30 +0200

    use prebuilt cryptopp lib

--- cmake/cryptopp.cmake
+++ cmake/cryptopp.cmake
@@ -8,7 +8,7 @@ endif ()
 include(FetchContent)
 
 FetchContent_Declare(cryptopp
-	GIT_REPOSITORY https://github.com/weidai11/cryptopp.git
+	GIT_REPOSITORY https://github.com/mramonlopez/cryptocpp-lib
 	GIT_TAG master
 )
 
@@ -16,22 +16,6 @@ FetchContent_GetProperties(cryptopp)
 
 if(NOT cryptopp_POPULATED)
 	FetchContent_Populate(cryptopp)
-
-	# Copy an additional/replacement file into the populated source
-	file(REMOVE_RECURSE ${cryptopp_SOURCE_DIR}/include)
-
-	file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/cmake/cryptopp/CMakeLists.txt DESTINATION ${cryptopp_SOURCE_DIR})
-
-	file(
-		COPY ${cryptopp_SOURCE_DIR}
-		DESTINATION ${cryptopp_SOURCE_DIR}/include
-		FILES_MATCHING 
-			PATTERN "*.h" 
-			PATTERN ".git*" EXCLUDE 
-			PATTERN "include" EXCLUDE 		
-	)
-
-	file(RENAME ${cryptopp_SOURCE_DIR}/include/cryptopp-src ${cryptopp_SOURCE_DIR}/include/cryptopp)
 	
 	add_subdirectory(${cryptopp_SOURCE_DIR} ${cryptopp_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif()
